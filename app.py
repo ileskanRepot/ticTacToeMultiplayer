4b802dd542d1fc23702018bf8983e69a5216bedf
Ileska <ileska@tutanota.com>
2023-01-24 16:50:06 +0200

    Simple chat client using websockets

--- app.py
+++ app.py
@@ -1,29 +1,34 @@
-from fastapi.responses import HTMLResponse
-from fastapi import (
-    FastAPI, WebSocket, WebSocketDisconnect,
-    Request
-)
 from typing import List
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-allData = []
 
-html = "Err"
-with open("./index.html","r") as f:
+html = ""
+with open('client.html', 'r') as f:
 	html = f.read()
 
 @app.get("/")
-async def root():
-    return HTMLResponse(content = html, status_code = 200)
+async def get():
+	return HTMLResponse(html)
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-				allData.append(data)
-        await websocket.send_text(f"Message text was: {data}")
+class ConnectionManager:
+	def __init__(self):
+		self.connections: List[WebSocket] = []
 
-@app.get("/code.js")
-async def js():
-    return {"message": "Hello World"}
+	async def connect(self, websocket: WebSocket):
+		await websocket.accept()
+		self.connections.append(websocket)
+
+	async def broadcast(self, data: str):
+		for connection in self.connections:
+			await connection.send_text(data)
+
+manager = ConnectionManager()
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+	await manager.connect(websocket)
+	while True:
+		data = await websocket.receive_text()
+		await manager.broadcast(f"Client {client_id}: {data}")
